cdc20610f65b01b4465508bdbec565950c5ec78e
NicholasNeto <nicholasneto@outlook.com>
2019-11-16 16:14:58 -0300

    created menuTree and menuItem

--- frontend/src/commun/templete/menu.jsx
+++ frontend/src/commun/templete/menu.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import MenuItem from '../templete/menu-item'
+import MenuTree from '../templete/menu-tree'
 
 
 export default props => (
-    <ul className='sidebar-menu'>
 
+    <ul className='sidebar-menu'>
+      <MenuItem path="/#/" label="Dashboard" icon='dashboard' />
+      <MenuTree label='Cadastro' icon='edit'>
+         <MenuItem path="#billingCycles" label="Ciclos de Pagamentos" icon='usd' />
+      </MenuTree>
     </ul>
 )
