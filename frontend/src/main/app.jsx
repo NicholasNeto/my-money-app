8072412c7e0ebf30f97a7969b4505159d957d300
NicholasNeto <nicholasneto@outlook.com>
2019-11-16 15:40:48 -0300

    implements menu and side-bar

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,10 +1,12 @@
 import '../commun/templete/dependencies'
 import Header from '../commun/templete/header'
+import SideBar from '../commun/templete/side-bar'
 import React from 'react'
 
 
 export default props => (
     <div className="wrapper">
         <Header  />
+        <SideBar />
     </div>
 )
