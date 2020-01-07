24083d40f9942172f0fb3d4372476fad7a6bfbf1
NicholasNeto <nicholasneto@outlook.com>
2020-01-06 21:11:33 -0300

    conenct redux with the project

--- frontend/src/commun/layout/grid.jsx
+++ frontend/src/commun/layout/grid.jsx
@@ -3,23 +3,22 @@ import React, { Component } from 'react'
 
 class Grid extends Component {
 
-    toCssClass(numbers){
+    toCssClass(numbers) {
         const cols = numbers ? numbers.split(' ') : []
         let classes = ''
 
-        if(cols[0]) classes +=  `cols-xs-${cols[0]}`
-        if(cols[1]) classes +=  `cols-sm-${cols[1]}`
-        if(cols[2]) classes +=  `cols-md-${cols[2]}`
-        if(cols[3]) classes +=  `cols-lg-${cols[3]}`
+        if (cols[0]) classes += `cols-xs-${cols[0]}`
+        if (cols[1]) classes += `cols-sm-${cols[1]}`
+        if (cols[2]) classes += `cols-md-${cols[2]}`
+        if (cols[3]) classes += `cols-lg-${cols[3]}`
 
         return classes
     }
 
-    render(){
+    render() {
 
-        const { cols } = this.props 
-        const gridClasses = this.toCssClass(cols || 12)
-        return(
+        const gridClasses = this.toCssClass(this.props.cols || 12)
+        return (
             <div className={gridClasses}>
                 {this.props.children}
             </div>
--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,18 +1,24 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
-import  CompoentHeader  from '../commun/templete/contentHeader'
-import  Content  from '../commun/templete/content'
+import CompoentHeader from '../commun/templete/contentHeader'
+import Content from '../commun/templete/content'
+import ValueBox from '../commun/widget/valueBox'
+import Row from '../commun/layout/row'
 
 
 class Dashboard extends Component {
 
-    render(){
+    render() {
 
         return (
             <div>
                 <CompoentHeader title={"Dashboard"} subTitle={"tetse"} />
                 <Content>
-                    Dashboard
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank' value=' R$ 10' tex='Total de Créditos' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card' value=' R$ 10' tex='Total de Débitos' />
+                        <ValueBox cols='12 4' color='blue' icon='money' value=' R$ 0' tex='Valor Consolidado' />
+                    </Row>
                 </Content>
             </div>
         )
