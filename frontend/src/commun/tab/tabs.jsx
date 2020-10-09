fc75f39f706887be02f1ab4213e088b35f6a0fef
NicholasNeto <nicholasneto@outlook.com>
2020-10-09 17:39:47 -0300

    create componet for tab Tabs - TabsHeader TabsContent

---
+++ frontend/src/commun/tab/tabs.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Tabs extends Component {
+
+    render() {
+        return (
+            <div className={'nav-tabs-custom'}>
+                {this.props.children}
+            </div>
+        )
+    }
+
+}
+
+export default Tabs
