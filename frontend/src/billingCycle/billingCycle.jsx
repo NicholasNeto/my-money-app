63371913730cd499ad3218ba09b7a550d418af9d
NicholasNeto <nicholasneto@outlook.com>
2020-10-09 17:40:41 -0300

    change component billingCycle for class component and used tabs components

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
+
+import ContentHeader from '../commun/templete/contentHeader'
+import Content from '../commun/templete/content'
 
 
-export default props => (
-    <div>
-        <h1>Ciclos de Pagamento</h1>
-    </div>
-)
+import Tabs from '../commun/tab/tabs'
+import TabsHeader from '../commun/tab/tabsHeader'
+import TabsContent from '../commun/tab/tabsContent'
+
+class BillingCycle extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { slected: null }
+    }
+
+    render() {
+        return (
+            <div>
+                <ContentHeader title={"Ciclos de Pagamentos"} small={"Cadastro"} />
+                <Content>
+                    <Tabs>
+                        <TabsHeader></TabsHeader>
+                        <TabsContent></TabsContent>
+                    </Tabs>
+
+                </Content>
+            </div>
+        )
+    }
+
+}
+
+export default BillingCycle
