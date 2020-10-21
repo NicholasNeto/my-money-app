fceca2665d883bba73a885771f643b7ffc679e4e
NicolsNeto-Finanzero <nicholas.pereira@finanzero.com.br>
2020-10-20 23:40:51 -0300

    implements action TAB_SHOWED

--- frontend/src/commun/tab/tab-actions.js
+++ frontend/src/commun/tab/tab-actions.js
@@ -5,3 +5,14 @@ export function selectTab(tabId) {
         payload: tabId
     }
 }
+
+
+export function showTabs(...tabIds){
+    const tabsToShow = {}
+    tabIds.forEach(tabId => tabsToShow[tabId] = true )
+
+    return {
+        type: 'TAB_SHOWED',
+        payload: tabsToShow
+    }
+}
