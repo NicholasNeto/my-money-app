14bad876cbdb32c99a5c62637191245ac6dd1b88
NicholasNeto <nicholasneto@outlook.com>
2020-10-26 21:15:59 -0300

    add colum action in billingCyclesList

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -10,6 +10,7 @@ class BillingCycleList extends Component {
                             <th>Nome</th>
                             <th>Mês</th>
                             <th>Ano</th>
+                            <th>Ações</th>
                         </tr>
                     </thead>
                     <tbody>
