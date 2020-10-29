825d77991f6dca24554686054fe49085b083cfc2
NicolsNeto-Finanzero <nicholas.pereira@finanzero.com.br>
2020-10-28 21:36:55 -0300

    fix in bindActionCreators

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,8 +1,8 @@
-import { axios } from 'axios'
+import  axios  from 'axios'
 
 const BASE_URL = 'http://localhost:3003/api/'
 
-export function getLIst(){
+export function getList(){
     const request = axios.get(`${BASE_URL}/billingCycles`)
     return {
         type: 'BILLING_CYCLES_FETCHED',
