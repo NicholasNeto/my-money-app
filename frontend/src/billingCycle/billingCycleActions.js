7f1b6861befbe4a78f9a7f7db18909c572a9554f
NicholasNeto <nicholasneto@outlook.com>
2020-11-03 21:53:01 -0300

    improve billingCycleActions

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
+import { reset as resetForm} from 'redux-form'
+import { showTabs, selectTab} from '../commun/tab/tab-actions'
 
 const BASE_URL = 'http://localhost:3003/api/'
 
@@ -12,14 +14,25 @@ export function getList() {
 }
 
 export function create(values) {
-    axios.post(`${BASE_URL}/billingCycles`, values)
-        .then((response) => {
-            toastr.success('Successo', 'Operação realizada com sucesso')
-        })
-        .catch((error) => {
-            Object.keys(error.response.data.errors)
-                .map(it => toastr.error('Error', error.response.data.errors[it].message))
-        })
+
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
+            .then((response) => {
+                toastr.success('Successo', 'Operação realizada com sucesso')
+                dispatch([
+                    resetForm('billingCycleForm'),
+                    getList(),
+                    selectTab('tabList'),
+                    showTabs('tabList', 'tabCreate')
+                ])
+            })
+            .catch((error) => {
+                Object.keys(error.response.data.errors)
+                    .map(it => toastr.error('Error', error.response.data.errors[it].message))
+            })
+    }
+
+
     return {
         type: 'TEMP',
     }
