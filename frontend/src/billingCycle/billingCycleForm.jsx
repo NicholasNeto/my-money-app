043b00e979945d55be5413857e880972d4cc4ac4
NicholasNeto <nicholasneto@outlook.com>
2020-12-30 06:28:11 -0300

    remvoed type input and implements labelAndInput

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
-import labelAndInput from '../commun/form/label-and-Input';
+import labelAndInput from '../commun/form/labelAndInput';
 
 class BillingCycleForm extends Component {
 
   render() {
-     const { handleSubmit } = this.props
+    const { handleSubmit } = this.props
 
     return (
       <form role="form" onSubmit={handleSubmit}>
         <div className='box-body'>
-          <Field name='name' component={'input'}/>
-          <Field name='month' component={'input'} />
-          <Field name='year' component={'input'} />
-
+          <Field
+            name='name'
+            component={labelAndInput}
+            cols={'12 4'}
+            placeholder={'Informe o nome'}
+            label='Nome'
+          />
+          <Field
+            name='month'
+            component={labelAndInput}
+            cols={'12 4'}
+            placeholder={'Informe o mês'}
+            type='number'
+            label='Mês'
+          />
+          <Field
+            name='year'
+            component={labelAndInput}
+            cols={'12 4'}
+            placeholder={'Informe o ano'}
+            type='number'
+            label='Ano' />
         </div>
         <div className='box-footer'>
           <button type='submit' className='btn btn-primary'>Submit</button>
