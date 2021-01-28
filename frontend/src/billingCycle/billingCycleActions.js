f76b5c0b669c2b033d96047ee457d71c0faf6341
NicholasNeto <nicholasneto@outlook.com>
2021-01-28 17:19:25 -0300

    set destroyOnUmmount false for billingCycleForm

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
-import { reset as resetForm} from 'redux-form'
+import { reset as resetForm, initialize} from 'redux-form'
 import { showTabs, selectTab} from '../commun/tab/tab-actions'
 
 const BASE_URL = 'http://localhost:3003/api/'
@@ -42,5 +42,6 @@ export function showUpdate(billingCycle){
     return [
         showTabs('tabUpdate'), 
         selectTab('tabUpdate'), 
+        initialize('billingCycleForm', billingCycle), 
     ]
 }
