04d9bdb029564699e88a7c1df09b63038f0cc2c4
NicholasNeto <nicholasneto@outlook.com>
2021-01-31 19:34:36 -0300

    implements component input

--- frontend/src/billingCycle/credit-list.jsx
+++ frontend/src/billingCycle/credit-list.jsx
@@ -2,14 +2,15 @@ import React, { Component } from 'react'
 import { Field } from 'redux-form'
 import Grid from '../commun/layout/grid'
 
+import Input from '../commun/form/input'
 
 class CreditList extends Component {
 
     renderRows() {
         return (
             <tr>
-                <td><Field name="credits[0].name" component='input' /></td>
-                <td><Field name="credits[0].value" component='input' /></td>
+                <td><Field name="credits[0].name" component={Input} placeholder='Informe o nome' readOnly={this.props.readOnly} /></td>
+                <td><Field name="credits[0].value" component={Input} placeholder='Informe o valor' readOnly={this.props.readOnly}/></td>
                 <td></td>
             </tr>
         )
