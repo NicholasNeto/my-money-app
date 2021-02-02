9fdb7d587bc95992fc78715c01af2d4875442601
NicholasNeto <nicholasneto@outlook.com>
2021-02-02 20:39:55 -0300

    implements func add, used arrayInsert from redux-form

--- frontend/src/billingCycle/credit-list.jsx
+++ frontend/src/billingCycle/credit-list.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
-import { Field } from 'redux-form'
+import { Field, arrayInsert } from 'redux-form'
 import Grid from '../commun/layout/grid'
+import { connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+
 
 import Input from '../commun/form/input'
 
+
 class CreditList extends Component {
+
+    add(index, item = {}) {
+        if(!this.props.readOnly){
+            this.props.arrayInsert("billingCycleForm", "credits", index, item)
+        } 
+    }
 
     renderRows() {
         const list = this.props.list || []
@@ -12,7 +22,18 @@ class CreditList extends Component {
             <tr key={index}>
                 <td><Field name={`credits[${index}].name`} component={Input} placeholder='Informe o nome' readOnly={this.props.readOnly} /></td>
                 <td><Field name={`credits[${index}].value`} component={Input} placeholder='Informe o valor' readOnly={this.props.readOnly} /></td>
-                <td></td>
+                <td>
+                    <button
+                        type="button"
+                        className="btn btn-success">
+                        <i className='fa fa-plus' onClick={() => this.add(index + 1)} />
+                    </button>
+                    <button
+                        type="button"
+                        className="btn btn-warning">
+                        <i className='fa fa-clone' onClick={() => this.add(index + 1, item)} />
+                    </button>
+                </td>
             </tr>
         ))
     }
@@ -42,4 +63,5 @@ class CreditList extends Component {
     }
 }
 
-export default CreditList
+const mapDispatchToProps = (dispatch) => bindActionCreators({ arrayInsert}, dispatch);
+export default connect(null, mapDispatchToProps)(CreditList);
