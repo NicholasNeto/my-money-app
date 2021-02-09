9df2f63f54bdb8d55f044c91e95a3b376bd79d2f
NicholasNeto <nicholasneto@outlook.com>
2021-02-09 00:45:20 -0300

    fix router

--- backend/src/config/router.js
+++ backend/src/config/router.js
@@ -10,15 +10,15 @@ module.exports = function (server) {
     const protectedApi = express.Router()
     server.use('/api', protectedApi)
     protectedApi.use(auth)
-    const BillingCycle = require('../api/billingCycle/billingCycleService')
+    const BillingCycle = require('../api/billingCycle/billingCycleServer')
     BillingCycle.register(protectedApi, '/billingCycles')
-    
+
     /*
     * Rotas abertas
     */
     const openApi = express.Router()
     server.use('/oapi', openApi)
-    const AuthService = require('../api/user/AuthService')
+    const AuthService = require('../api/user/authService')
     openApi.post('/login', AuthService.login)
     openApi.post('/signup', AuthService.signup)
     openApi.post('/validateToken', AuthService.validateToken)
