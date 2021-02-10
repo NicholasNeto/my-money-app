53ded8069c605ade16df2db5a255c3fb091709ac
NicholasNeto <nicholasneto@outlook.com>
2021-02-10 06:41:59 -0300

    create authReducer

--- frontend/src/main/reducers.jsx
+++ frontend/src/main/reducers.jsx
@@ -6,13 +6,15 @@ import { reducer as toastrdReducer } from 'react-redux-toastr'
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../commun/tab/tab-reducer'
 import BillingCycleReducer from '../billingCycle/billingCycle-reducer'
+import AuthReducer from '../auth/authReducer'
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
     tab : TabReducer,
     billingCycle: BillingCycleReducer,
     form: formReducer,
-    toastr: toastrdReducer
+    toastr: toastrdReducer,
+    auth: AuthReducer
 })
 
 export default rootReducer
