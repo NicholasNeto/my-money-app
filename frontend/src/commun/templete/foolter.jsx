a1081d00eb9bc10d048f5a0743b9fc788a7641b9
NicholasNeto <nicholasneto@outlook.com>
2021-02-16 21:36:33 -0300

    fix component foolter

--- frontend/src/commun/templete/foolter.jsx
+++ frontend/src/commun/templete/foolter.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-export default props => (
+const Foolter = (props) => (
     <footer className='main-footer'>
         <strong>
             Coyright &copy; 2017
-            <a href='http://cod3r.com.br' target='_blank'> Cod3r</a>
+            <a href='http://cod3r.com.br' rel="noreferrer noopener" target="_blank"> Cod3r</a>
         </strong>
     </footer>
 )
+
+export default Foolter
