caa29f7c13c3a2530abdc6b08f77922e9e8b3c57
NicholasNeto <nicholasneto@outlook.com>
2021-02-19 18:21:05 -0300

    fix component Form

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,73 +1,23 @@
-import React, { Component } from 'react';
-import { reduxForm, Field, formValueSelector } from 'redux-form';
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { init } from './billingCycleActions'
-import LabelAndInput from '../commun/form/labelAndInput';
-import ItemList from './itemList'
-import Summary from './summary'
-
+import React, { Component } from 'react'
+import { reduxForm, Field } from 'redux-form'
 class BillingCycleForm extends Component {
 
-  calculateSummary(credits, debts) {
-    const sum = (acumulador, currenteValue) => acumulador + currenteValue;
-    return {
-      sumOfCredits: credits.map(it => +it.value || 0).reduce(sum),
-      sumOfDebits: debts.map(it => +it.value || 0).reduce(sum),
-    }
-  }
-
   render() {
-    const { handleSubmit, readOnly, credits, debts } = this.props
-    const { sumOfCredits, sumOfDebits } = this.calculateSummary(credits, debts)
-    
+    const { handleSubmit } = this.props
     return (
-      <form onSubmit={handleSubmit}>
+      <form role='form' onSubmit={handleSubmit}>
         <div className='box-body'>
-          <Field
-            name='name'
-            component={LabelAndInput}
-            cols={'12 4'}
-            placeholder={'Informe o nome'}
-            label='Nome'
-            readOnly={readOnly}
-          />
-          <Field
-            name='month'
-            component={LabelAndInput}
-            cols={'12 4'}
-            placeholder={'Informe o mês'}
-            type='number'
-            label='Mês'
-            readOnly={readOnly}
-          />
-          <Field
-            name='year'
-            component={LabelAndInput}
-            cols={'12 4'}
-            placeholder={'Informe o ano'}
-            type='number'
-            label='Ano'
-            readOnly={readOnly}
-          />
-          <Summary credits={sumOfCredits} debts={sumOfDebits} />
-          <ItemList cols="12 6" list={credits} readOnly={readOnly} legend='Creditos' field='credits' />
-          <ItemList cols="12 6" list={debts} readOnly={readOnly} legend='Debitos' showStatus={true} field='debts' />
+          <Field name='name' component='input' />
+          <Field name='month' component='input' />
+          <Field name='year' component='input' />
         </div>
         <div className='box-footer'>
-          <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
-          <button type='button' className='btn btn-default' onClick={this.props.init} >Cancelar</button>
+          <button type='submit' className='btn btn-primary'
+          >Submit</button>
         </div>
       </form>
     )
   }
 }
 
-BillingCycleForm = reduxForm({ form: 'billingCycleForm', destroyOnUnmount: false })(BillingCycleForm)
-const selector = formValueSelector("billingCycleForm")
-const mapStateToProps = state => ({
-  credits: selector(state, 'credits'),
-  debts: selector(state, 'debts')
-})
-const mapDispatchToProps = dispatch => bindActionCreators({ init }, dispatch)
-export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleForm)
+export default reduxForm({ form: 'billingCycleForm' })(BillingCycleForm)
